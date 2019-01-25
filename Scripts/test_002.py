b6b4030c610b4cc1e4d33448d7e264479a645c45
qxy <2423939954@qq.com>
2019-01-25 20:33:57 +0800

    '初始化项目'

--- Scripts/test_002.py
+++ Scripts/test_002.py
@@ -1,12 +1,12 @@
 import allure
 
 class Test_002:
-    @allure.setp(title='我是test_003的操作步骤名称')
+    @allure.step(title='我是test_003的操作步骤名称')
     def test_003(self):
         print('----test_003')
         assert True
 
-    @allure.setp(title='我是test_003的操作步骤名称')
+    @allure.step(title='我是test_004的操作步骤名称')
     def test_004(self):
         print('----test_004')
         assert False
